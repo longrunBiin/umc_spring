f3c1d446670a69b1b38200a04e1ff9d49f15676c
jangsubin <eltm521@naver.com>
2023-12-13 21:18:21 +0900

    mission8

--- src/main/java/umc/spring/web/controller/TempRestController.java
+++ src/main/java/umc/spring/web/controller/TempRestController.java
@@ -1,7 +1,6 @@
 package umc.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.aspectj.apache.bcel.classfile.Code;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
